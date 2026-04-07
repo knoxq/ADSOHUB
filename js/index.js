64f61844403a6d291e6bf438bb43d8c2226b29a0
Miguel <angel13ganan@gmail.com>
2026-04-06 22:31:30 -0500

    Auto-commit (Miguel) - 2026-04-06 22:31

--- js/index.js
+++ js/index.js
@@ -16,13 +16,6 @@ async function cargarPeliculas() {
       <div class="group relative cursor-pointer">
         <img src="${IMG_SMALL + pelicula.poster_path}"
           class="rounded-lg group-hover:scale-105 transition duration-300">
-        <div class="absolute inset-0 bg-black/60 opacity-0 
-          group-hover:opacity-100 flex items-center justify-center transition">
-          <button onclick="obtenerTrailer('movie', ${pelicula.id})"
-            class="bg-[#E50914] px-4 py-2 rounded">
-            Ver tráiler
-          </button>
-        </div>
       </div>
     `;
   });
@@ -43,42 +36,9 @@ async function cargarSeries() {
       <div class="group relative cursor-pointer">
         <img src="${IMG_SMALL + serie.poster_path}"
           class="rounded-lg group-hover:scale-105 transition duration-300">
-        <div class="absolute inset-0 bg-black/60 opacity-0 
-          group-hover:opacity-100 flex items-center justify-center transition">
-          <button onclick="obtenerTrailer('tv', ${serie.id})"
-            class="bg-[#E50914] px-4 py-2 rounded">
-            Ver tráiler
-          </button>
-        </div>
       </div>
     `;
   });
-}
-
-/* OBTENER TRAILER */
-async function obtenerTrailer(tipo, id) {
-  const res = await fetch(
-    `https://api.themoviedb.org/3/${tipo}/${id}/videos?api_key=${API_KEY}&language=es-ES`
-  );
-  const data = await res.json();
-
-  const trailer = data.results.find(
-    video => video.type === "Trailer" && video.site === "YouTube"
-  );
-
-  if (trailer) {
-    document.getElementById("trailerFrame").src =
-      `https://www.youtube.com/embed/${trailer.key}`;
-
-    document.getElementById("trailerModal").classList.remove("hidden");
-    document.getElementById("trailerModal").classList.add("flex");
-  }
-}
-
-/* CERRAR TRAILER */
-function cerrarTrailer() {
-  document.getElementById("trailerModal").classList.add("hidden");
-  document.getElementById("trailerFrame").src = "";
 }
 
 cargarPeliculas();
